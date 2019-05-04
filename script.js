86c7ca35dab08c9ab2ee4166ba8b785b660eff86
Jeff <jmperkinson@gmail.com>
2019-05-04 07:06:21 -0400

    randomizer button added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
-var window = document.getElementsByTagName('window')[0];
+var window = document.querySelector('window');
+var button = document.querySelector('button');
 
 function backgroundGradient() {
     body.style.background = 
@@ -15,6 +16,19 @@ function backgroundGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomize() {
+    //TODO: Make this funciton better, slicing and bitwise?
+    //https://www.paulirish.com/2009/random-hex-color-code-snippets/
+    var a = '#' + Math.floor(Math.random() * 16777216).toString(16);
+    var b = '#' + Math.floor(Math.random() * 16777216).toString(16);
+    console.log(a);
+    console.log(b);
+    // color1.value = a;
+    // color2.value = b;
+    // backgroundGradient();
+}
+
 window.addEventListener("load", backgroundGradient);
 color1.addEventListener("input", backgroundGradient);
 color2.addEventListener("input", backgroundGradient);
+button.addEventListener("click", randomize);
